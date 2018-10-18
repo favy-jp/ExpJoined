31d2024aa69b0bdd7bdeee57e8c6f447726c6ccb
ketabawo <taku.obatake@monetize.co.jp>
2018-10-18 16:41:55 +0900

    update

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,7 +1,22 @@
 $(function(){
-  $('#js-bxSlider').bxSlider({
-    mode: 'fade',
-    speed: 2000,
-    auto: true
-  });
+  if(document.getElementById('js-bxSlider') != null){
+    $('#js-bxSlider').bxSlider({
+      mode: 'fade',
+      speed: 2000,
+      auto: true
+    });
+  }
+  if(document.getElementById('js-flexElementCompletion') != null){
+    var column = 5;
+    var addElement = column - $('.brandList__brand').length % column;
+    if(addElement < column){
+      var elementDOM = '';
+      for(var i = 1; i <= addElement; i++){
+        elementDOM = elementDOM + "<div class='brandList__brand'></div>";
+      }
+      $('#js-flexElementCompletion').append(elementDOM);
+    }
+  }
 });
+  
+
